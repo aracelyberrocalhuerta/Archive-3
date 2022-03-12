c585e3f5b65ee20da718397d13b9db189c49ced1
Aracely <79402005+aracelyberrocalhuerta@users.noreply.github.com>
2022-03-12 16:19:28 +0100

    Diseño Config

---
+++ App2/ConfigViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConfigViewController.swift
+//  App2
+//
+//  Created by user209640 on 3/12/22.
+//
+
+import UIKit
+
+class ConfigViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
